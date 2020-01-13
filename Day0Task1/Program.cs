b76c90573910edff828713cf68056134102bc199
Pavan-37 <pvnkmrsingh003@gmail.com>
2020-01-13 22:38:55 +0800

    Completed Day0Task1

---
+++ Day0Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HackerRank_30DaysOfCode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inputString = String.Empty;
+			inputString = Console.ReadLine();
+
+			Console.WriteLine("Hello, World.");
+			Console.WriteLine(inputString);
+		}
+	}
+}
