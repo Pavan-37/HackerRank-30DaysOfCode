1881d454c5d440d52a76a7d3d96136980b39ea09
Pavan-37 <pvnkmrsingh003@gmail.com>
2020-01-13 22:26:26 +0800

    Initialize HackerRank-30DaysOfCode solution

---
+++ DayTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HackerRank_30DaysOfCode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Console.ReadLine();
+		}
+	}
+}
